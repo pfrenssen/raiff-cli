456cc4260cc5cb75c6878ae3c059da809e059f0a
Pieter Frenssen <pieter@frenssen.be>
2016-05-07 13:54:59 +0300

    Ask the user for transactions.

--- src/Command/Transfer/InLeva.php
+++ src/Command/Transfer/InLeva.php
@@ -6,8 +6,10 @@ use Behat\Mink\Driver\Selenium2Driver;
 use Behat\Mink\Mink;
 use Behat\Mink\Session;
 use RaiffCli\Command\CommandBase;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\CssSelector\CssSelectorConverter;
 use Zumba\Mink\Driver\PhantomJSDriver;
 
@@ -21,12 +23,14 @@ class InLeva extends CommandBase
             ->setDescription('Do a bank transfer in leva.');
         $this->addAccountTypeArgument();
         $this->addAccountArgument();
+        $this->addArgument('transactions', InputArgument::REQUIRED, 'The transactions');
     }
 
     protected function interact(InputInterface $input, OutputInterface $output)
     {
         $this->askAccountType($input, $output);
         $this->askAccount($input, $output, $input->getArgument('account-type'));
+        $this->askTransactions($input, $output);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -81,6 +85,67 @@ class InLeva extends CommandBase
         } while ($timeout > 0);
 
         throw new \Exception("The element with selector '$selector' is not present on the page.");
+    }
+
+    /**
+     * Asks the user for transactions.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     *   The input interface.
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *   The output interface.
+     */
+    protected function askTransactions (InputInterface $input, OutputInterface $output) {
+        $transactions = [];
+
+        $helper = $this->getHelper('question');
+        while (true) {
+            // Ask recipient, limited to Bulgarian accounts.
+            $recipient = $this->askRecipient($input, $output, true, 'bulgaria');
+
+            // If the recipient is omitted, start the transactions.
+            if (empty($recipient)) {
+                if (empty($transactions)) {
+                    // No transactions yet. Restart.
+                    continue;
+                }
+                break;
+            }
+
+            // Ask amount in BGN.
+            $question = new Question('Amount in BGN: ');
+            $question->setValidator(function ($input) {
+                if (!empty($input) && preg_match("/^[0-9]+\.[0-9]{2}$/", $input) !== 1) {
+                    throw new \InvalidArgumentException('The amount must be in the format "123.45".');
+                }
+                return $input;
+            });
+            $amount = $helper->ask($input, $output, $question);
+
+            // Allow the user to start over by entering an empty amount.
+            if (empty($amount) || $amount == 0) {
+                $output->writeln('<comment>Skipping transaction with empty amount.</comment>');
+                continue;
+            }
+
+            // Ask for the description.
+            $question = new Question('Description: ');
+            $description = $helper->ask($input, $output, $question);
+
+            // Allow the user to start over by entering an empty description.
+            if (empty($description)) {
+                $output->writeln('<comment>Skipping transaction with empty description.</comment>');
+                continue;
+            }
+
+            $transactions[] = [
+                'recipient' => $recipient,
+                'amount' => $amount,
+                'description' => $description,
+            ];
+        }
+
+        $input->setArgument('transactions', $transactions);
     }
 
 }
