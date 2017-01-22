407e2de06bbc63cb84b599b8d5a1e614b7216e31
Pieter Frenssen <pieter@frenssen.be>
2017-01-22 16:32:36 +0200

    Refactor methods that are generally useful out of TransferBase.

--- src/Command/CommandBase.php
+++ src/Command/CommandBase.php
@@ -2,17 +2,109 @@
 
 namespace RaiffCli\Command;
 
+use Behat\Mink\Driver\Selenium2Driver;
+use Behat\Mink\Mink;
+use Behat\Mink\Session;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\CssSelector\CssSelectorConverter;
+use Zumba\Mink\Driver\PhantomJSDriver;
 
 /**
  * Base class for commands.
  */
 abstract class CommandBase extends Command
 {
+
+    /**
+     * The Mink session manager.
+     *
+     * @var \Behat\Mink\Mink
+     */
+    protected $mink;
+
+    /**
+     * The Mink session.
+     *
+     * @var \Behat\Mink\Session
+     */
+    protected $session;
+
+    /**
+     * Returns the dependency injection container helper.
+     *
+     * @return \RaiffCli\Helper\ContainerHelper
+     *   The dependency injection container helper.
+     */
+    protected function getContainer()
+    {
+        return $this->getHelper('container');
+    }
+
+    /**
+     * Returns the configuration manager.
+     *
+     * @return \RaiffCli\Config\ConfigManager
+     *   The configuration manager.
+     */
+    protected function getConfigManager()
+    {
+        return $this->getContainer()->get('config.manager');
+    }
+
+    /**
+     * Returns the Mink session manager.
+     *
+     * @return Mink
+     *   The Mink session manager.
+     */
+    protected function getMink()
+    {
+        if (empty($this->mink)) {
+            $this->mink = $this->initMink();
+        }
+        return $this->mink;
+    }
+
+    /**
+     * Returns the Mink session.
+     *
+     * @return Session
+     *   The Mink session.
+     */
+    protected function getSession()
+    {
+        return $this->getMink()->getSession();
+    }
+
+    /**
+     * Initializes Mink.
+     *
+     * @return Mink
+     *   The initialized Mink session manager.
+     */
+    protected function initMink()
+    {
+        $mink = new Mink();
+        $config = $this->getConfigManager()->get('config');
+
+        // Register PhantomJS driver.
+        $host = $config->get('mink.sessions.phantomjs.host');
+        $template_cache = $config->get('mink.sessions.phantomjs.template_cache');
+        if (!file_exists($template_cache)) mkdir($template_cache);
+        $mink->registerSession('phantomjs', new Session(new PhantomJSDriver($host, $template_cache)));
+
+        // Register Selenium driver.
+        $browser = $config->get('mink.sessions.selenium2.browser');
+        $host = $config->get('mink.sessions.selenium2.host');
+        $mink->registerSession('selenium2', new Session(new Selenium2Driver($browser, NULL, $host)));
+
+        $mink->setDefaultSessionName($config->get('mink.default_session'));
+        return $mink;
+    }
 
     /**
      * Adds an argument to the command for the account type.
@@ -174,25 +266,84 @@ abstract class CommandBase extends Command
     }
 
     /**
-     * Returns the dependency injection container helper.
+     * Waits for the element with the given selector to appear.
      *
-     * @return \RaiffCli\Helper\ContainerHelper
-     *   The dependency injection container helper.
+     * @param string $selector
+     *   The CSS selector identifying the element.
+     * @param string $engine
+     *   The selector engine name, either 'css' or 'xpath'. Defaults to 'css'.
+     *
+     * @throws \Exception
+     *   Thrown when the element doesn't appear within 10 seconds.
      */
-    protected function getContainer()
+    protected function waitUntilElementPresent($selector, $engine = 'css')
     {
-        return $this->getHelper('container');
+        $timeout = 20000000;
+        if ($engine === 'css') {
+            $converter = new CssSelectorConverter();
+            $selector = $converter->toXPath($selector);
+        }
+
+        do {
+            $elements = $this->session->getDriver()->find($selector);
+            if (!empty($elements)) return;
+            usleep(500000);
+            $timeout -= 500000;
+        } while ($timeout > 0);
+
+        throw new \Exception("The element with selector '$selector' is not present on the page.");
     }
 
     /**
-     * Returns the configuration manager.
-     *
-     * @return \RaiffCli\Config\ConfigManager
-     *   The configuration manager.
+     * Visits the homepage.
      */
-    protected function getConfigManager()
+    protected function navigateToHomepage()
     {
-        return $this->getContainer()->get('config.manager');
+        // Navigate to the homepage by clicking on the logo. We cannot visit the
+        // URL directly because we would lose session information passed by
+        // query arguments.
+        $this->session->getPage()->find('css', '#head a.logo')->click();
     }
+
+    /**
+     * Logs in.
+     */
+    protected function logIn()
+    {
+        $config = $this->getConfigManager()->get('config');
+        $base_url = $config->get('base_url');
+        $this->session->visit($base_url);
+        $this->session->getPage()->fillField('userName', $config->get('credentials.username'));
+        $this->session->getPage()->fillField('pwd', $config->get('credentials.password'));
+        $this->session->getPage()->find('css', '#m_ctrl_Page button.primary')->click();
+        $this->waitUntilElementPresent('#main .themebox.ind');
+    }
+
+    /**
+     * Selects the account type.
+     *
+     * @param string $account_type
+     *   The account type, either 'individual' or 'corporate'.
+     */
+    protected function selectAccountType($account_type)
+    {
+        $selector = $account_type === 'individual' ? '#main .themebox.ind a.btn.secondary' : '#main .themebox.corp a.btn.secondary';
+        $this->session->getPage()->find('css', $selector)->click();
+        $this->waitUntilElementPresent('#head a.logo');
+        // Close the marketing banner if it is present.
+        $this->closeCampaignContent();
+    }
+
+    /**
+     * Closes the marketing campaign dialog if it is present.
+     */
+    protected function closeCampaignContent()
+    {
+        $page = $this->session->getPage();
+        if ($page->find('css', '#CampaignContent')) {
+            $page->find('css', 'a.ui-dialog-titlebar-close')->click();
+        }
+    }
+
 
 }
